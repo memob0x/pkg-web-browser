82e52a51a5b70a484d07ee2c82867696bbec4b5b
memob0x <dfioroni91@gmail.com>
2022-05-03 19:19:12 +0200

    build fix attempt

--- build.js
+++ build.js
@@ -1,24 +1,27 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { resolve } from 'path';
-import { writeFile } from 'fs/promises';
+import { writeFile, mkdir } from 'fs/promises';
 
 import transpileScss from './src/js/transpile-scss';
 import rollupJs from './src/js/rollup-js';
 
-const buildStyle = async () => writeFile(
-  resolve('.', 'dist', 'style.css'),
+const srcPath = resolve('.', 'src');
+const distPath = resolve('.', 'dist');
 
-  await transpileScss(resolve('.', 'src', 'assets', 'style.scss')),
+const buildStyle = async () => writeFile(
+  resolve(distPath, 'style.css'),
+
+  await transpileScss(resolve(srcPath, 'assets', 'style.scss')),
 );
 
 const bundleScripts = async () => Promise.all([
   writeFile(
-    resolve('.', 'dist', 'scripts.js'),
+    resolve(distPath, 'scripts.js'),
 
     await rollupJs({
       input: {
-        input: './src/assets/scripts.js',
+        input: resolve(srcPath, 'assets', 'scripts.js'),
 
         plugins: [
           commonjs(),
@@ -36,11 +39,11 @@ const bundleScripts = async () => Promise.all([
   ),
 
   writeFile(
-    resolve('.', 'dist', 'launch-browser.cjs'),
+    resolve(distPath, 'launch-browser.cjs'),
 
     await rollupJs({
       input: {
-        input: './src/js/launch-browser.js',
+        input: resolve(srcPath, 'js', 'launch-browser.js'),
 
         plugins: [
           commonjs(),
@@ -58,11 +61,11 @@ const bundleScripts = async () => Promise.all([
   ),
 
   writeFile(
-    resolve('.', 'dist', 'index.cjs'),
+    resolve(distPath, 'index.cjs'),
 
     await rollupJs({
       input: {
-        input: './src/index.js',
+        input: resolve(srcPath, 'index.js'),
 
         plugins: [
           commonjs(),
@@ -89,6 +92,12 @@ const bundleScripts = async () => Promise.all([
     }),
   ),
 ]);
+
+try {
+  await mkdir(distPath);
+} catch (e) {
+  //
+}
 
 buildStyle();
 
